44295a5fc2f128057467fcd0e58f50a83a0fd91b
Ryan <zulloryan2005@gmail.com>
2023-06-07 09:52:05 +0200

    commit setting base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdottoComponent } from './component/prodotto/prodotto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdottoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/prodotto/prodotto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prodotto',
+  templateUrl: './prodotto.component.html',
+  styleUrls: ['./prodotto.component.css']
+})
+export class ProdottoComponent {
+
+}
