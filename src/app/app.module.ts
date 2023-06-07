ae16720efb7c88a69f75820f14076b943ae7a06d
fraaamoretti <francescomoretti292005@gmail.com>
2023-06-07 10:09:40 +0200

    ProdottiService fun

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdottoComponent } from './component/prodotto/prodotto.component';
+import { ProdottiService } from './service/prodottiService';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { ProdottoComponent } from './component/prodotto/prodotto.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProdottiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/service/prodottiService.ts
+++ src/app/service/prodottiService.ts
@@ -1,1 +1,10 @@
-//service
+import { Injectable } from "@angular/core";
+import { Prodotto } from "../model/prodotto";
+
+@Injectable()
+export class ProdottiService {
+    private libri: Prodotto[] = [
+        new Prodotto("La divina commedia", "Libri", 12, ['lalala'], 'libro bello bello'),
+        new Prodotto("I promessi sposi", "Libri", 14, ['lalala'], undefined)
+    ]
+}
