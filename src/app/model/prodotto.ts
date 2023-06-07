190a887245c70088af7d1faf8135aa56a0bb3864
Ryan <zulloryan2005@gmail.com>
2023-06-07 16:56:58 +0200

    adding elenco prodotti

--- src/app/model/prodotto.ts
+++ src/app/model/prodotto.ts
@@ -2,7 +2,7 @@ export enum Categorie{Mouse, Casa, Libri, Giocattoli}
 
 export class Prodotto
 {
-
+    path = '../../assets/img/';
     preferito = false;
 
     constructor(public nome:string, public categoria:Categorie, public prezzo:number, public img:string[], public descrizione?:string)
@@ -10,6 +10,11 @@ export class Prodotto
         if(this.descrizione === undefined)
         {
             this.descrizione = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum possimus, totam magni assumenda incidunt dolorem similique itaque sint beatae, inventore suscipit non excepturi esse eligendi rem nulla provident. Dignissimos, ipsa."
+        }
+
+        for(let i = 0; i < img.length; i++)
+        {
+            img[i] = this.path + img[i];
         }
     }
 }
