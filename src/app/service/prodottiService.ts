59e2cbf9d62a552298985a0aa0075c9e72ba4df0
Haojie Liu <=>
2023-06-08 12:47:45 +0200

    Added id

--- src/app/service/prodottiService.ts
+++ src/app/service/prodottiService.ts
@@ -1,23 +1,24 @@
 import { Injectable } from "@angular/core";
 import { Categorie, Prodotto } from "../model/prodotto";
 import { Carrello } from "../model/carrello";
+import { RouterTestingHarness } from "@angular/router/testing";
 
 @Injectable()
 export class ProdottiService {
     
     private magazzino: Prodotto[] = [
-        new Prodotto("Vita tra i funghi", Categorie.Fantasy, 31, ["fantasy1.png"], "Un libro di "),
-        new Prodotto("Musica dall'alto", Categorie.Fantasy, 24, ["fantasy2.png"], "Un libro di "),
-        new Prodotto("La biblioteca", Categorie.Giallo, 11, ["giallo1.png"], "Un libro di "),
-        new Prodotto("Dov'è Ambda", Categorie.Giallo, 22, ["giallo2.png"], "Un libro di "),
-        new Prodotto("Ecco qui Ambda", Categorie.Giallo, 27, ["giallo3.png"], "Un libro di "),
-        new Prodotto("L'amore non voluto", Categorie.Romanzo, 33, ["romanzo1.png"], "Un libro di "),
-        new Prodotto("Amore sul treno", Categorie.Romanzo, 20, ["romanzo2.png"], "Un libro di "),
-        new Prodotto("Viaggio su Giove", Categorie.Science, 27, ["science1.png"], "Un libro di "),
-        new Prodotto("Cosa c'è su Marte", Categorie.Science, 16, ["science2.png"], "Un libro di "),
-        new Prodotto("Atlantide nell'universo", Categorie.Science, 14, ["science3.png"], "Un libro di "),
-        new Prodotto("L'università di mio padre", Categorie.SelfDevelopment, 32, ["selfdevelopment1.png"], "Un libro di "),
-        new Prodotto("Indipendenti dalla natura", Categorie.SelfDevelopment, 19, ["selfdevelopment2.png"], "Un libro di ")
+        new Prodotto(1, "Vita tra i funghi", Categorie.Fantasy, 31, ["fantasy1.png"], "Un libro di "),
+        new Prodotto(2, "Musica dall'alto", Categorie.Fantasy, 24, ["fantasy2.png"], "Un libro di "),
+        new Prodotto(3, "La biblioteca", Categorie.Giallo, 11, ["giallo1.png"], "Un libro di "),
+        new Prodotto(4, "Dov'è Ambda", Categorie.Giallo, 22, ["giallo2.png"], "Un libro di "),
+        new Prodotto(5, "Ecco qui Ambda", Categorie.Giallo, 27, ["giallo3.png"], `Il continuo di "Dov'è Ambda" di `),
+        new Prodotto(6, "L'amore non voluto", Categorie.Romanzo, 33, ["romanzo1.png"], "Un libro di "),
+        new Prodotto(7, "Amore sul treno", Categorie.Romanzo, 20, ["romanzo2.png"], "Un libro di "),
+        new Prodotto(8, "Viaggio su Giove", Categorie.Science, 27, ["science1.png"], "Un libro di "),
+        new Prodotto(9, "Cosa c'è su Marte", Categorie.Science, 16, ["science2.png"], "Un libro di "),
+        new Prodotto(10, "Atlantide nell'universo", Categorie.Science, 14, ["science3.png"], "Un libro di "),
+        new Prodotto(11, "L'università di mio padre", Categorie.SelfDevelopment, 32, ["selfdevelopment1.png"], "Un libro di "),
+        new Prodotto(12, "Indipendenti dalla natura", Categorie.SelfDevelopment, 19, ["selfdevelopment2.png"], "Un libro di ")
     ]
 
     carrello: Carrello = new Carrello([]);
@@ -64,5 +65,12 @@ export class ProdottiService {
     getAll()
     {
         return this.carrello.listaProdotti;
+    }
+    getOne(wanted_id: number) : Prodotto | any
+    {
+        this.magazzino.forEach(p => {
+            if(p.id == wanted_id) return p;
+            else return;
+        })
     }
 }
